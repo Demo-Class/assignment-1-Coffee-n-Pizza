6e259700fe2060be3075c6622240fe9bec24bc0b
CHico-Lee <leechiho22@hotmail.com>
2018-01-30 11:34:50 -0500

    edit x

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
 	    featureX(); // call feature X.
+        System.out.println("x is the best.");
     }
 
     private static void featureX() {	// fixed typo:)
         // do something.
-        System.out.println("New Brunch2");
-        System.out.println("Hi This is Chico. Both x and y is the best");
+        System.out.println("do x");
     }
     private static void featureY() {	// fixed typo:(
         // do something.
