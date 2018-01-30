d6f950a96f77137c1251742ca9422968aad2a868
CHico-Lee <leechiho22@hotmail.com>
2018-01-30 09:56:48 -0500

    call feature x

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+	featureX(); // call feature X.
     }
 
     public void featureX() {	// fixed typo:)
