9448e4e9d9348d4a3d0ad9977e29a80575591ed5
CHico-Lee <leechiho22@hotmail.com>
2018-01-30 10:37:15 -0500

    change function to static

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,15 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-	featureX(); // call feature X.
+	    featureX(); // call feature X.
     }
 
-    public void featureX() {	// fixed typo:)
+    private static void featureX() {	// fixed typo:)
         // do something.
         System.out.println("New Brunch2");
         System.out.println("Hi This is Chico, Y is awesome");
     }
-    public void featureY() {	// fixed typo:(
+    private static void featureY() {	// fixed typo:(
         // do something.
     }
 }
