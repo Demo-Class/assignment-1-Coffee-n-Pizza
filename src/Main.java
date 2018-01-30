1ae911a450a91f3786c4214b2018daf7904304d5
CHico-Lee <leechiho22@hotmail.com>
2018-01-30 12:36:57 -0500

    initial demo

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-	    featureX(); // call feature X.
+	    feature();
     }
 
-    private static void featureX() {	// fixed typo:)
+    private static void feature() {
         // do something.
-        System.out.println("do x and y");
-    }
-    private static void featureY() {	// fixed typo:(
-        // do something.
+        System.out.println("Hello World!");
     }
 }
