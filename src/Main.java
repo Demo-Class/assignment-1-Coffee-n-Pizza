5450992f52dabdeb38be30f6199e7a76a2847bca
CHico-Lee <leechiho22@hotmail.com>
2018-01-31 14:24:54 -0500

    Initial Demo

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,11 @@
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("Y is the best feature");
-    }
-
-}
-
-
-/*
 
 public class Main {
 
     public static void main(String[] args) {
-	    feature();
-    }
-
-    private static void feature() {
-        // do something.
         System.out.println("Hello World!");
     }
+
+    private static void featureX() {
+        // do something.
+    }
 }
-*/
