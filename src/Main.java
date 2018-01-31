ca3554e272072ab6f1c57955f5cee7e2fc450fd9
CHico-Lee <leechiho22@hotmail.com>
2018-01-31 14:16:56 -0500

    merge x and y
    # Conflicts:
    #	src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("X is the best feature");
+        System.out.println("Y is the best feature");
     }
 
 }
