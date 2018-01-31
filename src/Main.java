6e110450fc249fb16ec084e99b332a116013c227
CHico-Lee <leechiho22@hotmail.com>
2018-01-31 17:13:12 -0500

    call feature x funciton

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,13 @@
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.println("Hello World!");
+        featureX();
     }
 
     private static void featureX() {
         // do something.
-        System.out.println("...");
+        System.out.println("x is no finish.");
     }
 }
